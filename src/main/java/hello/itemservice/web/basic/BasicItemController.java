e8fe51de2eb395c377812da1484408eb9e097ba8
JAEHYEOK <tmzkdlqpffl@gmail.com>
2024-05-02 22:04:09 +0900

    섹션07_상품 등록 처리 - ModelAttribute

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,10 +6,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -41,9 +38,44 @@ public class BasicItemController {
         return "/basic/addForm";
     }
 
+//    @PostMapping("/add") //addItemV2 만들기 위해 주석처리
+    public String addItemV1(@RequestParam String itemName, // form에서 post 방식으로 넘어오는 데이터 중 name=itemName 값을 받기 위한 어노테이션
+                       @RequestParam int price, // form에서 post 방식으로 넘어오는 데이터 중 name=prince 값을 받기 위한 어노테이션
+                       @RequestParam Integer quantity, 
+                       Model model) {
+
+        Item item = new Item(); // Item의 인스턴스 item 생성
+        item.setItemName(itemName); // @RequestParam으로 받아온 itemName의 값을 item 인스턴스의 itemName으로 저장
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item); // 생성 및 itemName, price, quantity를 설정한 item 인스턴스를 리포지토리에 저장
+
+        model.addAttribute("item", item); // item 인스턴스를 model에 전달
+
+        return "/basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) { // 3. Model model은 생략해도 됨
+        
+        // 1. @ModelAttribute 가 form에서 넘어온 데이터를 받아서 자동으로 item 인스턴스를 생성하고, set을 해주므로 인스턴스 생성과 set 코드 생략
+
+        itemRepository.save(item);
+//        model.addAttribute("item", item); // 2. @ModelAttribute 어노테이션이 어노테이션의 name 속성으로 지정한 이름(여기서는 item)으로 model 객체도 넘겨주기 때문에 생략 가능
+        return "/basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) { // 어노테이션의 name 속성을 생략하면 클래스(Item)의 첫 글자를 소문자로 바꿔서(item) modelAttribute 에 넣어줌
+        itemRepository.save(item);
+        return "/basic/item";
+    }
+
     @PostMapping("/add")
-    public String save() {
-        return "/basic/addForm";
+    public String addItemV4(Item item) { // 생략 시, String 타입같은 것이 오면 @RequestParam 자동 적용, 사용자 정의 타입의 클래스가 오면 @ModelAttribute 자동 적용
+        itemRepository.save(item);
+        return "/basic/item";
     }
 
     /**
